b11fbe0e14271d4dcb8ebb6955908767dc09561c
Ben Darlow <github@kapowaz.net>
2022-04-18 23:50:41 +0100

    Update config with new set of rules, removing old ones

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-/**
- * This is the eslint config for linting the eslint-config.
- * We do this so our eslint config itself follows the rules
- * it outlines.
- */
-module.exports = require('./index.js');
